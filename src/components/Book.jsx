e8d601551432ee288fd770aaf4e88c902f502778
R-Srijanki <jankirathod999@gmail.com>
2025-11-06 19:18:18 +0530

    book component

---
+++ src/components/Book.jsx
@@ -0,0 +1,24 @@
+import './style.css'
+export default function Book({book}){
+    return(
+        <div className="w-50 sm:w-58 bg-white rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 overflow-hidden">
+      <img
+        src={book.imageUrl}
+        alt={book.title}
+        className="w-full h-64 object-cover"
+      />
+      <div className="p-3 text-center">
+        <h3 className="text-base font-semibold text-gray-800 truncate">
+          {book.title}
+        </h3>
+        <p className="text-sm text-gray-500 italic mb-2">{book.author}</p>
+         <p className="text-sm text-yellow-600 font-semibold mb-2">
+        ⭐ Rating: {book.irating}/5
+      </p>
+      </div>
+    </div>
+       
+    )
+}
+
+
